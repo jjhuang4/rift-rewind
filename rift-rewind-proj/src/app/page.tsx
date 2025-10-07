a4361adfb57d50b36e214d1d6a689c55fca241db
jjhuang4 <jjhuang4@illinois.edu>
2025-10-06 22:26:51 -0500

    test for ssr

--- rift-rewind-proj/src/app/page.tsx
+++ rift-rewind-proj/src/app/page.tsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
-export async function getServerSideProps() {
-  return { props: { time: new Date().toISOString() } };
-}  
-
-export default function Home({ time }: { time: string }) {
+export default async function Home() {
+  const time = new Date().toISOString();
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-bold">Rift Rewind</h1>
-      <p>The time is {time}</p>
-    </div>
+    <main>
+      <h1>It's Rift Rewind time!</h1>
+      <p>At: {time}!</p>
+    </main>
   );
 }
+
